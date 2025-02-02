69387c25fc66730f77caf22632a8cc654bd69ac4
Ahmad Wahab <aw865386@gmail.com>
2025-02-02 22:16:49 +0500

    home page compelted

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function()
+{
+  var typed = new Typed('#typed', {
+    strings: ["Web Devlopment", "FrontEnd Development","Graphic Designing","UI/UX Designing"],
+    typeSpeed: 50,
+    startDelay: 500,
+    backDelay: 800,
+    backSpeed: 40,
+    loop: true,
+  });
+});
